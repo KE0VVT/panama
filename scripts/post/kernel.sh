d69728474508cbe8e055bee40ae3abc8cb0609ab
Caleb Herbert <csh@bluehome.net>
2023-05-31 16:22:52 -0500

    Use JosSamLoh's replace-kernel.sh

--- scripts/post/kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-# Your code goes here.
-rpm-ostree override replace https://linux-libre.fsfla.org/pub/linux-libre/rpmfreedom/latest/RPMS/x86_64/kernel-6.3.5_gnu-1.x86_64.rpm
