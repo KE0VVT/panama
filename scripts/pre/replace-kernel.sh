7fa4b70d5a9f8f0ed364c0ab8b0e51078314094f
Caleb Herbert <csh@bluehome.net>
2023-05-31 18:14:43 -0500

    Add license headers

--- scripts/pre/replace-kernel.sh
+++ scripts/pre/replace-kernel.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-# Taken from
-# https://github.com/JosSamLoh/silverblue-fsync/blob/7eae76273e27a99b0eac8a73746880b5b2459247/scripts/pre/replace-kernel.sh
+# Copyright 2023 JosSamLoh
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Tell this script to exit if there are any errors.
 set -oue pipefail
